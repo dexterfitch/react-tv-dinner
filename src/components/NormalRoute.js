aff64dede8675761e05507256059fb05624e395f
dexterfitch <fitch.dexter@gmail.com>
2021-04-16 18:19:09 -0700

    Base

---
+++ src/components/NormalRoute.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NormalRoute() {
+  return (
+    <div className='w-11/12 max-w-6x1 mx-auto mt-8 text-2x1'>
+      This is a normal route, any one can see it.
+    </div>
+  )
+}
